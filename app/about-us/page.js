b335f940e8e2b13d8c886bc28594b8ac82b78c41
Sumit Tandon <sumit.tandon@oattravel.com>
2025-10-01 12:30:16 +0530

    Navigation Exercise

---
+++ app/about-us/page.js
@@ -0,0 +1,7 @@
+export default function AboutUs(){
+    return (
+        <main>
+        <h1> About Us</h1>
+        </main>
+    );
+}
--- app/page.js
+++ app/page.js
@@ -1,9 +1,12 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main>
       <img src="/logo.png" alt="A server surrounded by magic sparkles." />
       <h1>Welcome to this NextJS Course!</h1>
       <p>🔥 Let&apos;s get started! 🔥</p>
+      <Link href= "/about-us" > About Us</Link>
     </main>
   );
 }
