f013e2bfc29b5b6283c28183e89a6b6c7d9e3d3c
Sumit Tandon <sumit.tandon@oattravel.com>
2025-11-26 12:34:49 +0530

    Added Layout for Blog Page

---
+++ app/blog/layout.js
@@ -0,0 +1,8 @@
+export default function BlogLayout({children}){
+    return (
+        <>
+        <h1> Blog Layout </h1>
+        {children}
+        </>
+    )
+}
--- app/blog/page.js
+++ app/blog/page.js
@@ -4,8 +4,8 @@ export default function BlogPostPage(){
     return (
         <main>
             <h1> Blog Post Page</h1>
-            <Link href="/blog/post-1" > Post 1</Link>
-            <Link href="/blog/post-2" > Post 2</Link>
+           <p> <Link href="/blog/post-1" > Post 1</Link></p>
+           <p> <Link href="/blog/post-2" > Post 2</Link></p>
         </main>
     )
 }
