b212572a6b7d62ce520f565bdbe79d4be342051d
Sumit Tandon <sumit.tandon@oattravel.com>
2025-12-03 01:17:24 +0530

    Initial Committ

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,8 @@
-import './globals.css'
+import './globals.css';
 
 export const metadata = {
-  title: 'NextJS Course App',
-  description: 'Your first NextJS app!',
+  title: 'Next Auth',
+  description: 'Next.js Authentication',
 };
 
 export default function RootLayout({ children }) {
--- app/page.js
+++ app/page.js
@@ -1,9 +1,5 @@
-export default function Home() {
-  return (
-    <main>
-      <img src="/logo.png" alt="A server surrounded by magic sparkles." />
-      <h1>Welcome to this NextJS Course!</h1>
-      <p>🔥 Let&apos;s get started! 🔥</p>
-    </main>
-  );
+import AuthForm from '@/components/auth-form';
+
+export default async function Home() {
+  return <AuthForm />;
 }
